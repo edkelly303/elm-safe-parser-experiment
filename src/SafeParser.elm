ea5296fc3945476a93bf59294bd43d56ac41a750
Ed Kelly <edwardjkelly@gmail.com>
2026-01-01 19:42:45 +0000

    Reorder functions

--- src/SafeParser.elm
+++ src/SafeParser.elm
@@ -25,23 +25,13 @@ type Parser constraints a
     = P (ElmParser.Parser a)
 
 
-type Step state a
-    = Loop state
-    | Done a
-
-
-type Chomps
-    = Chomps Never
-
-
 run : Parser constraints a -> String -> Result (List ElmParser.DeadEnd) a
 run (P p) string =
     ElmParser.run p string
 
 
-backtrackable : Parser constraints a -> Parser constraints a
-backtrackable (P p) =
-    P (ElmParser.backtrackable p)
+type Chomps
+    = Chomps Never
 
 
 chompIf : (Char -> Bool) -> Parser chomps ()
@@ -54,29 +44,39 @@ chompWhile test =
     P (ElmParser.chompWhile test)
 
 
-oneOf : List (Parser constraints a) -> Parser constraints a
-oneOf list =
-    P (ElmParser.oneOf (List.map (\(P p) -> p) list))
-
-
 succeed : a -> Parser (Maybe Chomps) a
 succeed a =
     P (ElmParser.succeed a)
 
 
-done : Parser Chomps a -> Parser chomps (Step state a)
-done (P p) =
-    P (ElmParser.map Done p)
+keep0 : Parser (Maybe Chomps) a -> Parser constraints (a -> b) -> Parser constraints b
+keep0 (P x) (P f) =
+    P (f |= x)
 
 
-unsafelyDone : Parser (Maybe Chomps) a -> Parser chomps (Step state a)
-unsafelyDone (P p) =
-    P (ElmParser.map Done p)
+keep : Parser Chomps a -> Parser constraints (a -> b) -> Parser chomps b
+keep (P x) (P f) =
+    P (f |= x)
 
 
-continue : Parser constraints state -> Parser constraints (Step state a)
-continue =
-    map Loop
+skip0 : Parser (Maybe Chomps) skip -> Parser constraints keep -> Parser constraints keep
+skip0 _ (P keeper) =
+    P keeper
+
+
+skip : Parser Chomps skip -> Parser constraints keep -> Parser chomps keep
+skip _ (P keeper) =
+    P keeper
+
+
+oneOf : List (Parser constraints a) -> Parser constraints a
+oneOf list =
+    P (ElmParser.oneOf (List.map (\(P p) -> p) list))
+
+
+backtrackable : Parser constraints a -> Parser constraints a
+backtrackable (P p) =
+    P (ElmParser.backtrackable p)
 
 
 map : (a -> b) -> Parser constraints a -> Parser constraints b
@@ -113,24 +113,9 @@ andThenImplementation f (P p) =
     P (ElmParser.andThen unwrappedF p)
 
 
-keep0 : Parser (Maybe Chomps) a -> Parser constraints (a -> b) -> Parser constraints b
-keep0 (P x) (P f) =
-    P (f |= x)
-
-
-keep : Parser Chomps a -> Parser constraints (a -> b) -> Parser chomps b
-keep (P x) (P f) =
-    P (f |= x)
-
-
-skip0 : Parser (Maybe Chomps) skip -> Parser constraints keep -> Parser constraints keep
-skip0 _ (P keeper) =
-    P keeper
-
-
-skip : Parser Chomps skip -> Parser constraints keep -> Parser chomps keep
-skip _ (P keeper) =
-    P keeper
+type Step state a
+    = Loop state
+    | Done a
 
 
 loop : state -> (state -> Parser Chomps (Step state a)) -> Parser chomps a
@@ -153,3 +138,18 @@ loop state callback =
                     )
     in
     P (ElmParser.loop state unwrappedCallback)
+
+
+continue : Parser constraints state -> Parser constraints (Step state a)
+continue =
+    map Loop
+
+
+done : Parser Chomps a -> Parser chomps (Step state a)
+done (P p) =
+    P (ElmParser.map Done p)
+
+
+unsafelyDone : Parser (Maybe Chomps) a -> Parser chomps (Step state a)
+unsafelyDone (P p) =
+    P (ElmParser.map Done p)
